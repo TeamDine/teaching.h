054ef5d0e76c49536cc1c0f8149b0183b5c88278
AlexAlvrzCastell <39219220+AlexAlvrzCastell@users.noreply.github.com>
2018-05-20 10:12:53 -0500

    Actualización de la clase
    Se cambia atributo nombre (tipo Name) a (tipo String)

--- teaching.h
+++ teaching.h
@@ -1,16 +1,17 @@
-///Cabecera de los Dependientes económicos
+///Cabecera de Docencia
 #ifndef TEACHING_H_INCLUDED
 #define TEACHING_H_INCLUDED
 
+/*** Cabeceras del sistema ***/
+#include <string>
 
 /*** Cabeceras locales ***/
-#include <string>
 #include "name.h"
 #include "date.h"
 
 class Teaching{
     private:
-        Name name;
+        std::string name;
         Date initialDate;
         Date finishDate;
         int hours;
@@ -18,22 +19,26 @@ class Teaching{
     public:
         Teaching();                                   ///Constructor base
         Teaching(const Teaching&);                   /// Constructor Copia
-        Teaching(const Name&, Date&, Date&, int&);    ///Constructor Parametrizado
+        Teaching(const std::string&, Date&, Date&, int&);    ///Constructor Parametrizado
 
         /*** getters ***/
-        Name getName();
+        std::string getName();
         Date getInitialDate();
         Date getFinishDate();
         int getHours();
 
         /*** setters ***/
-        void setName(const Name&);
+        void setName(const std::string&);
         void setInitialDate(const Date&);
         void setFinishDate(const Date&);
         void setHours(const int&);
 
         std::string toString();     ///Imprime datos
 };
+
+
+#endif // TEACHING_H_INCLUDED
+
 
 
 #endif // TEACHING_H_INCLUDED
